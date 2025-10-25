d60482036d6d58985a06e1bdb5ada25d1b64ccfe
shweta roy <shwetaroy@shwetas-MacBook-Air.local>
2025-10-25 16:54:33 +0530

    html-pdf -generator

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module go-web-scraper
 
 go 1.25.0
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20250803210736-d308e07a266d // indirect
+	github.com/chromedp/chromedp v0.14.2 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250910080747-cc2cfa0554c3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,42 +1,77 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
-	
+	"time"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/cdproto/page"
+
 )
 
 func createFileName(url string) string {
 	url = strings.Replace(url, "/", "_", -1)
 
-	return url + ".html"
+	return url + ".pdf"
 }
 
 func main() {
 	url := "https://vim.rtorr.com/"
+	outputFile := createFileName(url)
 
-	resp, err := http.Get(url)
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var pdfData []byte
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			var err error
+			pdfData,_, err = page.PrintToPDF().
+				WithPrintBackground(true). // include background colors/images
+				WithPaperWidth(8.27).      // A4 width
+				WithPaperHeight(11.69).    // A4 height
+				Do(ctx)
+			return err
+		}),
+	)
+
 	if err != nil {
-		fmt.Println("Error fetching the URL:", err)
-	}
-
-	defer resp.Body.Close()
-	fileName := createFileName(url)
-	file,err := os.Create(fileName)
-	if(err!=nil){
-		fmt.Println("Error creating the file:", err)
+		fmt.Println("Error generating PDF:", err)
 		return
 	}
-	defer file.Close()
-	
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		fmt.Println("Error saving the file:", err)
+    if err := os.WriteFile(outputFile, pdfData, 0644); err != nil {
+		fmt.Println("❌ Error saving PDF:", err)
+		return
 	}
+	
 
-	fmt.Println("File saved successfully as", fileName)
+// resp, err := http.Get(url)
+	// if err != nil {
+	// 	fmt.Println("Error fetching the URL:", err)
+	// }
+
+	// defer resp.Body.Close()
+	// fileName := createFileName(url)
+	// file,err := os.Create(fileName)
+	// if(err!=nil){
+	// 	fmt.Println("Error creating the file:", err)
+	// 	return
+	// }
+	// defer file.Close()
+
+	// _, err = io.Copy(file, resp.Body)
+	// if err != nil {
+	// 	fmt.Println("Error saving the file:", err)
+	// }
+
+	fmt.Println("File saved successfully as", outputFile)
 
 }
